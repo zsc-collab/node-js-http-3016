f440dac2c206fccad1730b2a9e8953dae7b23e75
zsc-collab <243454441+zsc-collab@users.noreply.github.com>
2026-04-23 07:11:38 +0900

    D:/Program Files/Git/ や /enquetes にアクセスしたときの処理を追加

--- index.js
+++ index.js
@@ -11,29 +11,44 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-tofu') {
-          res.write(
+        if (req.url === '/') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケートフォーム</h1>' +
+            '<a href="/enquetes">アンケート一覧</a>' +
+            '</body></html>');
+        } else if (req.url === '/enquetes') {
+          res.write('<!DOCTYPE html><html lang="ja"><body>' +
+            '<h1>アンケート一覧</h1><ul>' +
+            '<li><a href="/enquetes/yaki-tofu">焼き肉・湯豆腐</a></li>' +
+            '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
+            '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
+            '</ul></body></html>');
+        } 
+        switch (req.url){ 
+          case '/enquetes/yaki-tofu':
+            res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
               firstItem: '焼き肉',
               secondItem: '湯豆腐'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
+            }));
+            break;
+          case '/enquetes/rice-bread':
+            res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
               firstItem: 'ごはん',
               secondItem: 'パン'
-            })
-          );
-        } else if (req.url === '/enquetes/sushi-pizza') {
-          res.write(pug.renderFile('./form.pug', {
+            }));
+          break;
+          case '/enquetes/sushi-pizza':
+            res.write(pug.renderFile('./form.pug', {
             path: req.url,
             firstItem: '寿司',
             secondItem: 'ピザ'
           }));
-        }
+          break;
+            }
         res.end();
         break;
       case 'POST':
