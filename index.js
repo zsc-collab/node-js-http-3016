83232d2bf1921d23d406a0b457ecbaa410d603a4
zsc-collab <243454441+zsc-collab@users.noreply.github.com>
2026-04-23 07:32:19 +0900

    ログから日付の削除

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const server = http
           .on('end', () => {
             const answer = new URLSearchParams(rawData);
             const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
-            console.info(`[${now}] ${body}`);
+            console.info(`[${body}`);
             res.write(`<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`);
             res.end();
           });
